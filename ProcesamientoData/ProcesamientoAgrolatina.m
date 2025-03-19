7094c9e4e89a4ee95b4b6e770647d237756a7dfa
Pedroart <pedroarteta@hotmail.es>
2025-03-19 00:13:49 -0500

    Procesamiento de Datos

---
+++ ProcesamientoData/ProcesamientoAgrolatina.m
@@ -0,0 +1,88 @@
+%% Import data from text file
+
+%% Set up the Import Options and import the data
+opts = delimitedTextImportOptions("NumVariables", 21, "Encoding", "UTF-8");
+
+% Specify range and delimiter
+opts.DataLines = [2, Inf];
+opts.Delimiter = ";";
+
+% Especificar nombres de columnas y tipos de datos
+opts.VariableNames = ["FechaHora", "TemperaturaAmbiente", "AperturaValvula", "Caudal", "EstadoSistema", ...
+                      "PresionPall", "TemperaturaRetorno", "SensorZona1", "SensorZona2", "SensorZona3", ...
+                      "SensorZona4", "SensorZona5", "SensorZona6", "SensorZona7", "SensorZona8", ...
+                      "SensorZona9", "SensorZona10", "SensorZona11", "SensorZona12", "TemperaturaSetpoint", ...
+                      "Ventilacion"];
+opts.VariableTypes = ["datetime", "double", "double", "double", "double", "double", "double", ...
+                      "double", "double", "double", "double", "double", "double", "double", ...
+                      "double", "double", "double", "double", "double", "double", "double"];
+
+% Especificar propiedades a nivel de archivo
+opts.ExtraColumnsRule = "ignore";
+opts.EmptyLineRule = "read";
+
+% Especificar propiedades de las variables
+opts = setvaropts(opts, "FechaHora", "InputFormat", "dd/MM/yyyy HH:mm:ss");
+
+% Variables que deben procesarse como numéricas y sin caracteres no numéricos
+varsToClean = ["TemperaturaAmbiente", "TemperaturaRetorno", "SensorZona1", "SensorZona2", ...
+               "SensorZona3", "SensorZona4", "SensorZona5", "SensorZona6", "SensorZona7", ...
+               "SensorZona8", "SensorZona9", "SensorZona10", "SensorZona11", "SensorZona12"];
+
+opts = setvaropts(opts, varsToClean, "TrimNonNumeric", true);
+opts = setvaropts(opts, varsToClean, "ThousandsSeparator", ",");
+
+% Import the data
+
+% Especificar la carpeta donde están los archivos CSV
+folderPath = "C:\Users\pedro\Documents\proyectos\Modelamiento-Temperatura\ProcesamientoData\Agrolatina\";
+
+% Obtener la lista de archivos CSV en la carpeta
+csvFiles = dir(fullfile(folderPath, "*.csv"));
+
+% Crear una estructura para almacenar las tablas
+dataTables = struct();
+
+% Leer cada archivo y almacenarlo en la estructura
+for i = 1:length(csvFiles)
+    % Obtener el nombre completo del archivo
+    fileName = csvFiles(i).name;
+    fullFilePath = fullfile(folderPath, fileName);
+
+    % Crear un nombre de variable válido (sin espacios ni caracteres inválidos)
+    %varName = matlab.lang.makeValidName(erase(fileName, ".csv"));
+    varName = sprintf("tunel_%d", i)
+    
+    % Leer el archivo CSV en una tabla
+    tbl = readtable(fullFilePath, opts);
+    
+    % Rellenar los valores NaN con el valor más cercano (Interpolación)
+    for col = 2:width(tbl) % Saltar la primera columna (FechaHora)
+        tbl{:, col} = fillmissing(tbl{:, col}, 'nearest');
+    end
+    
+    % Guardar la tabla en la estructura
+    dataTables.(varName) = tbl;
+
+    % Mostrar mensaje de progreso
+    fprintf("Archivo leído: %s\n", fileName);
+end
+
+% Mostrar el contenido de la estructura con las tablas cargadas
+disp(dataTables);
+
+Agrolatina = dataTables;
+
+%% Clear temporary variables
+clear opts
+clear varNames
+clear fileName
+clear folderPath
+clear fullFilePath
+clear i
+clear tbl
+clear varName
+clear col
+clear dataTables
+clear csvFiles
+clear varsToClean
